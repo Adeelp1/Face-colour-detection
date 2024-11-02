1ac9dd18611d1b47398effb7c04e7af189ce61bc
adeel <adeelpv360@gmail.com>
2024-11-02 02:06:23 -0700

    Add face detection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
  
+#Face detection
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
 # Below function will read video imgs
 cap = cv2.VideoCapture(0)
  
@@ -14,6 +17,7 @@ while True:
     input_image_cpy = img.copy()
  
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # define range of yellow color in HSV
     lower_yellow = (25,100,100)
@@ -80,9 +84,19 @@ while True:
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.putText(img, 'Blue', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    
+    # detect MultiScale and draw a rectangle around them
+    faces = face_cascade.detectMultiScale(gray,1.3,4)
+
+    for x,y,w,h in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,0), 4)
+        cv2.putText(img, 'Face', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,255,0), 2)
   
     cv2.imshow('Color Recognition Output', img)
      
     # Close video window by pressing 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
